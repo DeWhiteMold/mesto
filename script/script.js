01e871f817d10d5e613f1db22fe8a0c6b57e7df8
Nikolai Smirnov <dewhitemold@yandex.ru>
2022-10-22 13:46:09 +0400

    fix 1st revue

--- script/script.js
+++ script/script.js
@@ -1,56 +1,32 @@
-let popUp = document.querySelector('.profile__pop-up');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.profile__close-button');
-let saveButton = document.querySelector('.profile__save-button');
-let nameInput = document.querySelector('.profile__edit-name');
-let descripInput = document.querySelector('.profile__edit-description');
 let profileName = document.querySelector('.profile__name');
-let profileDecrip = document.querySelector('.profile__description');
-let overlay = document.querySelector('.profile__edit-overlay');
+let profileDescription = document.querySelector('.profile__description');
+let nameInput = document.querySelector('.pop-up__input_type_name');
+let descriptionInput = document.querySelector('.pop-up__input_type_description');
 
-editButton.addEventListener('click', switchPopUp);
-closeButton.addEventListener('click', switchPopUpNoSave);
-saveButton.addEventListener('click', submitInput);
-overlay.addEventListener('click', switchPopUpNoSave);
+let editForm = document.querySelector('.pop-up__edit-form');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.pop-up__close-button');
+let overlay = document.querySelector('.pop-up__overlay');
+let popUp = document.querySelector('.pop-up');
 
-
-function switchPopUp() {
-  popUp.classList.toggle('profile__pop-up');
+function showPopUp() {
+  popUp.classList.remove('pop-up_visability');
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
 }
 
-function switchPopUpNoSave() {
-  nameInput.value = '';
-  descripInput.value = '';
-  switchPopUp();
+function hidePopUp() {
+  popUp.classList.add('pop-up_visability');
 }
 
-function submitInput() {
-  if(nameInput.value === '' && descripInput.value === '') {
-    switchPopUpNoSave();
-  }
-  else if(nameInput.value === ''){
-    profileDecrip.innerText = descripInput.value;
-    descripInput.value = '';
-    descripInput.setAttribute('placeholder', profileDecrip.innerText);
-    switchPopUp();
-  }
-  else if(descripInput.value === '') {
-    profileName.innerText = nameInput.value;
-    nameInput.value = '';
-    nameInput.setAttribute('placeholder', profileName.innerText);
-    switchPopUp();
-  }
-  else {
-    profileName.innerText = nameInput.value;
-    profileDecrip.innerText = descripInput.value;
-    nameInput.value = '';
-    descripInput.value = '';
-    nameInput.setAttribute('placeholder', profileName.innerText);
-    descripInput.setAttribute('placeholder', profileDecrip.innerText);
-    switchPopUp();
-  }
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.innerText = nameInput.value;
+  profileDescription.innerText = descriptionInput.value;
+  hidePopUp();
 }
 
-
-
-
+editButton.addEventListener('click', showPopUp);
+popUp.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', hidePopUp);
+overlay.addEventListener('click', hidePopUp);
